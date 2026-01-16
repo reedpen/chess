87a162c82b6526176833f6d5e435b203bf9a228e
Reed Pennock <reederpen@gmail.com>
2026-01-16 10:31:24 -0700

    changed implementation to work easily with 1 indexing

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -10,8 +10,8 @@ public class ChessPosition {
     private final int row;
     private final int col;
     public ChessPosition(int row, int col) {
-        this.row = row;
-        this.col = col;
+        this.row = row-1;
+        this.col = col-1;
     }
 
     /**
