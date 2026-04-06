b64992860c083bb37c978f5d3c558b58eb165f6e
Reed Pennock <reederpen@gmail.com>
2026-04-06 09:57:09 -0600

    added message subclasses

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,30 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+public class ErrorMessage extends ServerMessage {
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ErrorMessage)) return false;
+        if (!super.equals(o)) return false;
+        ErrorMessage that = (ErrorMessage) o;
+        return Objects.equals(getErrorMessage(), that.getErrorMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getErrorMessage());
+    }
+}
---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,31 @@
+package websocket.messages;
+
+import chess.ChessGame;
+import java.util.Objects;
+
+public class LoadGameMessage extends ServerMessage {
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LoadGameMessage)) return false;
+        if (!super.equals(o)) return false;
+        LoadGameMessage that = (LoadGameMessage) o;
+        return Objects.equals(getGame(), that.getGame());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getGame());
+    }
+}
